d389db7e785bb7c10e40af37bdc902109053d4c9
unknown <paolo.moser2199@gmail.com>
2021-06-16 13:33:39 +0200

    errors

--- src/main/java/it/uniroma3/siw/spring/controller/validator/CollezioneValidator.java
+++ src/main/java/it/uniroma3/siw/spring/controller/validator/CollezioneValidator.java
@@ -1,7 +1,5 @@
 package it.uniroma3.siw.spring.controller.validator;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -22,8 +20,10 @@ public class CollezioneValidator implements Validator{
 	@Override
 	public void validate(Object o, Errors errors) {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nome", "required");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "curatore", "required");
-
+		//ValidationUtils.rejectIfEmptyOrWhitespace(errors, "curatore", "required");
+		
+		if( ((Collezione)o).getCuratore() == null)
+			errors.rejectValue("curatore", "required");
 		if (!errors.hasErrors()) {
 			if (this.collezioneService.alreadyExists((Collezione)o)) {
 				errors.reject("collezione.duplicato");
